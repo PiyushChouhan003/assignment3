59e754fe013016f02994bc391bbb9c77fb58201b
PiyushChouhan003 <chouhanpiyush003@gmail.com>
2023-06-19 08:06:39 +0530

    first commit

---
+++ ForthWhile.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ForthWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the numer");
+        int num = sc.nextInt();
+        int i=1;
+        int sum =0;
+        while (i<= 10) {
+          System.out.println( num+" * "+i+" = "+i*num);
+            i++;
+        }
+    }
+}
